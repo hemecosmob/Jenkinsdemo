6fae1f076a2aea0ccf0759fb24ad71879b5b462f
hemecosmob <hemdip.badani@ecosmob.com>
2023-04-18 15:45:02 +0530

    adding message

--- helloword.java
+++ helloword.java
@@ -1,4 +1,4 @@
 public class Simple{
 public static void main(String args[]){
-System.out.println("hello this is first program for git to jenkins");
+System.out.println("hello this is first program for git to jenkins-1");
 }}
